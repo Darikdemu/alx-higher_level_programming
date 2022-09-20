4cf0d0dc97c4db73397bbfc170d23c7689c382a8
Darik <darikdmsw@gmail.com>
2022-09-20 21:04:26 +0000

    python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def slower(c):
-    ascii_num = ord(c)
-    if ascii_num >= 97 and ascii_num <= 122:
+def islower(c):
+
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+
         return True
-    return False
+
+    else:
+
+        return Falsen
