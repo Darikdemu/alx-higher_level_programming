ad8db396ca9e9acd89286ad198c82f4df607c414
Darik <darikdmsw@gmail.com>
2022-09-21 18:33:52 +0000

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        print('{:d}{:d}'.format(i, j), '\n' if (i == 8 and j == 9) else ', ',
-              sep='', end='')
+for number in range(100):
+    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
+        print("{}{}".format(int(number / 10), number % 10), end="")
+        if (number != 89):
+            print(", ", end="")
+print("")
