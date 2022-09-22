e5950dfa7ba8438838560b3e6cd36917028f4efc
Darik <darikdmsw@gmail.com>
2022-09-22 11:56:49 -0700

    0x01

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-number = 97
-while(number < 123):
-if 113 == number or 101 == number:
-number += 1
-continue
-print("{:s}".format(chr(number)), end='')
-number += 1
+for in range(97, 123):
+
+    if i != 101 and i != 113:
+
+        print("{:c}".format(i), end="")
