177f5717e27d1298a0d117f35a9d0c59d94e204b
Darik <darikdmsw@gmail.com>
2022-09-22 12:01:32 -0700

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        print('{:d}{:d}'.format(i, j), '\n' if (i == 8 and j == 9) else ', ',
-              sep='', end='')
+for n in range(0, 100):
+
+    if n < 99:
+
+        print("{:02d}, ".format(n), end='')
+
+    else:
+
+        print("{:02d}".format(n))
