cbdae4f22da61fb12431d1d70e8b703e12dd1c6b
Darik <darikdmsw@gmail.com>
2022-09-28 18:39:53 +0000

    prints all integers of a list.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-<<<<<<< HEAD
         print("{d}".format(my_list[i])
-=======
         print("{:d}".format(my_list[i]))
->>>>>>> 39ab55494a120afbe71621445aadee9a32e5caf0
