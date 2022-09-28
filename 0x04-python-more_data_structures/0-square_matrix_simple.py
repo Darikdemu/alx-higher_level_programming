3e099bb4786fb22a4d467574650b69140a0bcf8b
Darik <darikdmsw@gmail.com>
2022-09-28 14:54:33 +0000

    the square value of all integers of a matrix.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sq = []
-    for elem in matrix:
-        sq.append([x**2 for x in elem ])
-    return(sq)
+
+
+    squared = []
+
+    for line in matrix:
+
+        squared.append([c**2 for c in line])
+
+    return squared
