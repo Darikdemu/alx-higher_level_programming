bf0c49e780fe88b55d0bf1229307a28e95ae6c28
Darik <darikdmsw@gmail.com>
2022-10-19 08:28:18 -0700

    all tasks

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-"""Thisdefines a locked class"""
+"""This defines a locked class"""
 class LockedClass:
-    """
-    Only allows instatiation of an attribute called first_name
+    """    Only allows instatiation of an attribute called first_name
     """
     __slots__ = ["first_name"]
