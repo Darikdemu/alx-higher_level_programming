c846817eede23560779279587c5ac38bb565c4df
Darik <darikdmsw@gmail.com>
2022-10-24 07:41:07 -0700

    python-inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+contains the class BaseGeometry
+"""
+
+class BaseGeometry:
+
+    """An empty class"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains the class BaseGeometry
+"""
+
+class BaseGeometry:
+
+    """A class with public attribute area"""
+
+    def area(self):
+
+        """raises an exception when called"""
+
+        raise Exception("area() is not implemented")
