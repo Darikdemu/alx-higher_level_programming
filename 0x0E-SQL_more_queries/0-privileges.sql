ddca253490fcdad65fc86928e46adcbb2ca6d065
Darik <darikdmsw@gmail.com>
2022-12-01 03:16:50 -0800

    my privilage

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- Creates the MySQL server user: user_0d_1
-CREATE USER IF NOT EXISTS 'usr_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* O 'user_0d_1'@'localhost'
+--lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_1@localhost;
+-- lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_2@localhost;
