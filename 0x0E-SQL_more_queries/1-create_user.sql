7995a6f7258269e4d898ccd11c689967a24a3abd
Darik <darikdmsw@gmail.com>
2022-12-02 04:43:12 -0800

    SQL_more_queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,4 @@
 -- all privileges.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL ON *.*
-TO 'user_0d_1'@'localhost'
+GRANT ALL PREVILAGES ON *.* TO 'user_0d_1'@'localhost'
