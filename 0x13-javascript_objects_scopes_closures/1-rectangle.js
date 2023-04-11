015bc6e54cf4066d35ea1bf0ec8c5412256542c8
Darik <darikdmsw@gmail.com>
2023-04-11 10:59:53 +0000

    task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-class Rectangle{ 
-    constructor (h, w){
+class Rectangle { 
+    constructor (h, w) {
         this.width = w;
         this.height = h;
     }
 }
+module.exports = Rectangle;
