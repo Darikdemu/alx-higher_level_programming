2cb144a444b6677cf97ba04d033509309599657b
Darik <darikdmsw@gmail.com>
2023-04-11 12:29:51 +0000

    task 6 done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
-#1/usr/bin/node
-import square from './5-square'
+#!/usr/bin/node
+const square = require('./5-square');
 
 class Square extends square {
-    charprint(c){
-        if(c === undefined){
-            console.log(('X'.repeat(this.Width) + '\n').repeat(this.height-1) + 'X'.repeat(this.height))
-        }
-        else{
-            console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    } else {
+      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
     }
-} 
+  }
+}
+module.exports = Square;
