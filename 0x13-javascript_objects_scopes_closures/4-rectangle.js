27de58d32d2e44cab770a990afd091c11854c1a0
Darik <darikdmsw@gmail.com>
2023-04-11 11:31:08 +0000

    4-rec

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
-class Rectangle{ 
-    constructor (h, w){
-         if (((w = parseInt(w)) <= 0) && ((h = parseInt(h)) <= 0)) {
-            this.width = w;
-            this.height = h;
-         }
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    print(){
-        console.log(('X'.repeat(this.width) + '\n').repeat(this.height-1) + 'X'.repeat(this.width));
-    }
-    rotate(){
-        const h = this.height;
-        const w = this.width;
-        this.height =w;
-        this.width = h;
-    }
-    double(){
-        this.height *= 2;
-        this.width *= 2;
-    }
-    
+  }
+
+  print () {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+  }
+
+  rotate () {
+    const h = this.height;
+    const w = this.width;
+    this.width = h;
+    this.height = w;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
 }
+module.exports = Rectangle;
