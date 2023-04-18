518d4f60df2eb49ec9d90d002dfad82f41dcd3d3
Darik <darikdmsw@gmail.com>
2023-04-18 07:18:51 -0700

    lil modification

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-"""
-Changes the name of a State object from the database hbtn_0e_6_usa
-"""
+'''Script to list all State objects that contain the
+letter a from the database hbtn_0e_6_usa'''
+
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+if __name__ == "__main__":
+    if len(sys.argv) > 3:
+        user_name = sys.argv[1]
+        passwd = sys.argv[2]
+        db_name = sys.argv[3]
 
-    stateUpdated = session.query(State).filter(State.id == 2).first()
+        engine = create_engine(f"mysql+mysqldb:\
+//{user_name}:{passwd}@localhost:3306/{db_name}")
 
-    if stateUpdated:
-        stateUpdated.name = 'New Mexico'
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        to_update = session.query(State).filter(State.id == '2').first()
+
+        to_update.name = 'New Mexico'
+
         session.commit()
+    else:
+        print(f"Usage: ./11-model_state_insert.py \
+        <mysql_username> <mysql_password> <database_name>")
